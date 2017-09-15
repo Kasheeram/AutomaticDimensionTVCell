272f54ebe407f56ce1c87736747fa4b6c06d4341
Apogee <kashee ram kushwaha>
2017-09-15 10:45:27 +0530

    Initial commit

---
+++ AutomaticDimensionTVCell/AutomaticDimensionTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  AutomaticDimensionTableViewCell.swift
+//  AutomaticDimensionTVCell
+//
+//  Created by Apogee on 9/15/17.
+//  Copyright © 2017 Apogee. All rights reserved.
+//
+
+import UIKit
+
+class AutomaticDimensionTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
